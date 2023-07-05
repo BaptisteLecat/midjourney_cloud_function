69319e7a9adad6731fd56447210c186df2734d1d
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-07-06 01:22:54 +0200

    Add comment and remove unused property in generated image

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,22 +2,29 @@ import * as functions from "firebase-functions";
 import { Generation } from "./entities/generation.entity";
 import { ImageGeneratorService } from "./services/image_generator.service";
 
-//pub sub trigger for when a new generation is created
+//firestore trigger for when a new generation is created
 export const onGenerationCreated = functions.runWith({ timeoutSeconds: 100 }).region("europe-west1").firestore.document('users/{userId}/generations/{generationId}').onCreate(async (snapshot, context) => {
     const generationId = context.params.generationId;
     const generation = Generation.fromFirestoreDocument(generationId, snapshot.data());
     let imageGeneratorService: ImageGeneratorService = new ImageGeneratorService();
+
+    //Register progress callback
     imageGeneratorService.onProgress(async (progress: string) => {
+        //Save progress to firestore
         progress = progress.replace('%', '');
         generation.progress = parseInt(progress);
         console.log(generation.toFirestoreDocument());
         await snapshot.ref.set(generation.toFirestoreDocument());
     });
+
+    //Generate image
     await imageGeneratorService.generateImage(generation.prompt).then(async (generatedImage) => {
         if (generatedImage == null || generatedImage == undefined) {
             console.log("generatedImage is null or undefined");
             throw new Error("Unable to generate image");
         }
+
+        //Save image to firestore
         generation.generatedImage = generatedImage;
         generation.progress = 100;
         console.log(generation.toFirestoreDocument());
